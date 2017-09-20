e7f4089349adf6085722d3c60320405e617e2a6a
apple <2571583272@qq.com>
2017-09-20 21:53:18 +0800

    增加 访问网页 gzip 支持

--- baiduUtil/http_request.go
+++ baiduUtil/http_request.go
@@ -1,9 +1,9 @@
 package baiduUtil
 
 import (
+	"bytes"
 	"crypto/tls"
 	"io/ioutil"
-	"log"
 	"net/http"
 	"net/http/cookiejar"
 	"net/url"
@@ -23,30 +23,34 @@ func HTTPGet(urlStr string) (body []byte, err error) {
 //Fetch 实现 http／https 访问 和 GET／POST 请求，根据给定的 urlStr (网址), jar (cookie容器 指针), post (post数据 指针), header (请求头数据 指针) 进行网站访问。
 //返回值分别为 网站主体, 错误
 func Fetch(urlStr string, jar *cookiejar.Jar, post, header map[string]string) (body []byte, err error) {
-	URL, err := url.Parse(urlStr)
-	if err != nil {
-		log.Fatalln(err)
-	}
-
 	var req *http.Request
 	httpClient := &http.Client{Timeout: 3e10} // 30s
 	if jar != nil {
 		httpClient.Jar = jar
 	}
 
-	if URL.Scheme == "https" {
+	if HTTPSRE.MatchString(urlStr) {
 		httpClient.Transport = &http.Transport{TLSClientConfig: &tls.Config{InsecureSkipVerify: true}}
 	}
 
+	if _, ok := header["Accept-Encoding"]; !ok {
+		header["Accept-Encoding"] = "gzip"
+	}
 	if post == nil {
-		req, _ = http.NewRequest("GET", urlStr, nil)
+		req, err = http.NewRequest("GET", urlStr, nil)
+		if err != nil {
+			return nil, err
+		}
 		addHeader(req, header)
 	} else {
 		query := url.Values{}
 		for k, v := range post {
 			query.Set(k, v)
 		}
-		req, _ = http.NewRequest("POST", urlStr, strings.NewReader(query.Encode()))
+		req, err = http.NewRequest("POST", urlStr, strings.NewReader(query.Encode()))
+		if err != nil {
+			return nil, err
+		}
 		addHeader(req, header)
 	}
 	resp, err := httpClient.Do(req)
@@ -54,7 +58,13 @@ func Fetch(urlStr string, jar *cookiejar.Jar, post, header map[string]string) (b
 		return nil, err
 	}
 	defer resp.Body.Close()
-	return ioutil.ReadAll(resp.Body)
+
+	body, _ = ioutil.ReadAll(resp.Body)
+	undatas, err := DecompressGZIP(bytes.NewReader(body))
+	if err != nil {
+		return body, nil
+	}
+	return undatas, nil
 }
 
 func addHeader(req *http.Request, header map[string]string) {
