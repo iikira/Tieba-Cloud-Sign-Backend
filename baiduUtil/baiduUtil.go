77a866d78202d23f914079d943cc7d6b25c32cec
apple <2571583272@qq.com>
2017-09-25 19:55:19 +0800

    分工

--- baiduUtil/baiduUtil.go
+++ baiduUtil/baiduUtil.go
@@ -5,13 +5,12 @@ import (
 	"bytes"
 	"compress/gzip"
 	"crypto/md5"
+	"flag"
 	"fmt"
 	"io"
 	"io/ioutil"
-	"log"
 	"net/http/cookiejar"
 	"net/url"
-	"os"
 	"sort"
 	"strings"
 	"time"
@@ -124,17 +123,17 @@ func DecompressGZIP(r io.Reader) ([]byte, error) {
 	return ioutil.ReadAll(gzipReader)
 }
 
-// PrintErrIfExist 简易错误处理, 如果 err 存在, 就只向屏幕输出 err 。
-func PrintErrIfExist(err error) {
-	if err != nil {
-		log.Println(err)
+// FlagProvided 检测命令行是否提供名为 name 的 flag, 支持多个name(names)
+func FlagProvided(names ...string) bool {
+	var targetFlag *flag.Flag
+	for _, name := range names {
+		targetFlag = flag.Lookup(name)
+		if targetFlag == nil {
+			return false
+		}
+		if targetFlag.DefValue == targetFlag.Value.String() {
+			return false
+		}
 	}
-}
-
-// PrintErrAndExit 简易错误处理, 如果 err 存在, 向屏幕输出 err 并退出, annotate 是加在 err 之前的注释信息。
-func PrintErrAndExit(annotate string, err error) {
-	if err != nil {
-		log.Println(annotate, err)
-		os.Exit(1)
-	}
+	return true
 }
